47fbcf64a1cd42efeda10147410c8664df969e83
Luc <lucmcgrady@gmail.com>
2021-10-12 08:23:40 +0100

    Replaced: findable files with urls

--- src/quizNSquestions.js
+++ src/quizNSquestions.js
@@ -9,5 +9,115 @@
 	},
 ]*/
 
-const questions = [{ "question": "Identify The Dessert", "possible": ["Coffee Cake", "Chocolate Cake", "pie", "pizza"], "answer": "Chocolate Cake", "img": "chocolatecake.jpg" }, { "question": "Identify The Dessert", "possible": ["pumpkin Cake", "Orange Tart", "Orange pie", "pumpkin pie"], "answer": "pumpkin pie", "img": "pumkinpie.jpg" }, { "question": "What Topping Do The Brownies Have?", "possible": ["Meringue", "Custard", "Cream", "Ice Cream"], "answer": "Cream", "img": "https://media-cldnry.s-nbcnews.com/image/upload/t_focal-860x430,f_auto,q_auto:best/newscms/2017_07/1195780/food-elizabeth-heiskell-red-velvet-brownies-mississippi-mud-tease-today-170216-01.jpg" }, { "question": "Identify The Dessert", "possible": ["Macaroons", "Small Cookies", "Macaron", "Fancy Cookies"], "answer": "Macaroons", "img": "macaroon.jpg" }, { "question": "What Flavour Is The Muffin?", "possible": ["Chcocolate", "Caramel", "Fudge", "Coffee"], "answer": "Coffee", "img": "Coffeee.jpg" }, { "question": "What Flavour Is The Ice Cream?", "possible": ["Chocolate", "CHCOCOLaTE", "Vanilla", "Bubble Gum"], "answer": "Vanilla", "img": "sundae.jpg" }, { "question": "Identify The Dessert", "possible": ["Key Lime pie", "apple Tart", "Lime Tart", "Sweet apple pie"], "answer": "Key Lime pie", "img": "KeyLimepie.jpg" }, { "question": "What Kind Of Dessert Is this", "possible": ["pie", "Tart", "Meringue", "Cake"], "answer": "Meringue", "img": "Lemonmerinihe.jpg" }, { "question": "Identify The Dessert", "possible": ["Raspberry Jelly", "Raspberry Tart", "Strawberry Tart", "Berry Jelly"], "answer": "Strawberry Tart", "img": "Strawtart.jpg" }, { "question": "What Flavour Is The Cake", "possible": ["Marble", "Coffee", "Chocolate", "Vanilla"], "answer": "Marble", "img": "Marblecake.jpg" }]
-
+const questions = [
+	{
+	  "question": "Identify The Dessert",
+	  "possible": [
+		"Coffee Cake",
+		"Chocolate Cake",
+		"pie",
+		"pizza"
+	  ],
+	  "answer": "Chocolate Cake",
+	  "img": "http://www.softsteps.me/images/CAKE.jpg"
+	},
+	{
+	  "question": "Identify The Dessert",
+	  "possible": [
+		"pumpkin Cake",
+		"Orange Tart",
+		"Orange pie",
+		"pumpkin pie"
+	  ],
+	  "answer": "pumpkin pie",
+	  "img": "https://lh3.googleusercontent.com/ieyN2H7_GJzbrofjEC4zpBhrYFbE2QySTCNp8ZIZBj3sW7HUHCkIW2WR5pMZKnEtPL-acYdpN5igKdzI_rICyA=w1280-h1280-c-rw-v1-e365"
+	},
+	{
+	  "question": "What Topping Do The Brownies Have?",
+	  "possible": [
+		"Meringue",
+		"Custard",
+		"Cream",
+		"Ice Cream"
+	  ],
+	  "answer": "Cream",
+	  "img": "https://media-cldnry.s-nbcnews.com/image/upload/t_focal-860x430,f_auto,q_auto:best/newscms/2017_07/1195780/food-elizabeth-heiskell-red-velvet-brownies-mississippi-mud-tease-today-170216-01.jpg"
+	},
+	{
+	  "question": "Identify The Dessert",
+	  "possible": [
+		"Macaroons",
+		"Small Cookies",
+		"Macaron",
+		"Fancy Cookies"
+	  ],
+	  "answer": "Macaroons",
+	  "img": "macaroon.jpg"
+	},
+	{
+	  "question": "What Flavour Is The Muffin?",
+	  "possible": [
+		"Chcocolate",
+		"Caramel",
+		"Fudge",
+		"Coffee"
+	  ],
+	  "answer": "Coffee",
+	  "img": "Coffeee.jpg"
+	},
+	{
+	  "question": "What Flavour Is The Ice Cream?",
+	  "possible": [
+		"Chocolate",
+		"CHCOCOLaTE",
+		"Vanilla",
+		"Bubble Gum"
+	  ],
+	  "answer": "Vanilla",
+	  "img": "https://static.wixstatic.com/media/fe011c_4a9da80143714691abda6829aad60fff~mv2.jpg/v1/fill/w_1000,h_881,al_c,q_85/fe011c_4a9da80143714691abda6829aad60fff~mv2.jpg"
+	},
+	{
+	  "question": "Identify The Dessert",
+	  "possible": [
+		"Key Lime pie",
+		"apple Tart",
+		"Lime Tart",
+		"Sweet apple pie"
+	  ],
+	  "answer": "Key Lime pie",
+	  "img": "KeyLimepie.jpg"
+	},
+	{
+	  "question": "What Kind Of Dessert Is this",
+	  "possible": [
+		"pie",
+		"Tart",
+		"Meringue",
+		"Cake"
+	  ],
+	  "answer": "Meringue",
+	  "img": "Lemonmerinihe.jpg"
+	},
+	{
+	  "question": "Identify The Dessert",
+	  "possible": [
+		"Raspberry Jelly",
+		"Raspberry Tart",
+		"Strawberry Tart",
+		"Berry Jelly"
+	  ],
+	  "answer": "Strawberry Tart",
+	  "img": "Strawtart.jpg"
+	},
+	{
+	  "question": "What Flavour Is The Cake",
+	  "possible": [
+		"Marble",
+		"Coffee",
+		"Chocolate",
+		"Vanilla"
+	  ],
+	  "answer": "Marble",
+	  "img": "Marblecake.jpg"
+	}
+  ]
