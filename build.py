584a5bb2d57d34de19c7283f50e672be0e9ea2d8
Luc <lucmcgrady@gmail.com>
2021-10-14 12:49:50 +0100

    Build now checks subfolders

--- build.py
+++ build.py
@@ -27,18 +27,18 @@ def render(getpath, putpath, midfunc, *args):
 		with open(putpath, "w", encoding="utf8") as f:
 			f.write(contents)
 
+RESERVED_DIRS = [ "!templates", "!template_vals" ]
 
 def build(src="src", dest="build"):
 	"""
 	This is designed to make it easier to format files to export them to App Inventor.
-	the folder "src" can only contain files and no directories as
-	 any files contained in the subdirectories will not be copied.
-	This is because there are no directories in App Inventor.
 
 	Every template in the templates folder should have a corresponding json file of the same name in the templates folder which will be used to
-	render the template with django
+	render the template with jinja2
 
 	"""
+	if not os.path.isdir(dest):
+		os.mkdir(dest)
 
 	for filename in os.listdir(src):
 		get_path = f"{src}/{filename}"
@@ -51,15 +51,25 @@ def build(src="src", dest="build"):
 				with open(get_path, "rb") as r, open(put_path, "wb") as w:
 					w.write(r.read())
 
-	for filename in os.listdir(f"{src}/templates"):
-		put_path = f"{dest}/{filename}"
-		get_path = f"{src}/templates/{filename}"
-		get_cfg_path = f"{src}/template_vals/{'.'.join(filename.split('.')[:-1])}.json"
+	template_path = f"{src}/!templates"
+	if os.path.isdir(template_path):
+		for filename in os.listdir(template_path):
+			put_path = f"{dest}/{filename}"
+			get_path = f"{src}/!templates/{filename}"
+			get_cfg_path = f"{src}/!template_vals/{'.'.join(filename.split('.')[:-1])}.json"
 
-		with open(get_cfg_path, "r", encoding="utf8") as f:
-			cfg = json.load(f)
+			with open(get_cfg_path, "r", encoding="utf8") as f:
+				cfg = json.load(f)
 
-		render(get_path, put_path, render_template, cfg)
+			render(get_path, put_path, render_template, cfg)
+
+	for dir in os.listdir(src):
+		path = f"{src}/{dir}"
+
+		if not os.path.isdir(path) or dir in RESERVED_DIRS:
+			continue
+
+		build(path) # Recursive for folders inside the build
 
 
 if __name__ == "__main__":
