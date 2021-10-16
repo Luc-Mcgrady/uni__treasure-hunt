15fcdac5043a3393aa41f8ceadbb5fbe8131446b
Luc <lucmcgrady@gmail.com>
2021-10-16 19:39:03 +0100

    Added: App inventor url fetch

--- src/API.js
+++ src/API.js
@@ -21,3 +21,25 @@ function setDBkey(key, value) {
 	}
 	genericAPI(req)
 }
+
+function* _entriesOf(object) {
+	for (i in object) 
+		yield [i, object[i]]
+}
+
+function getURL(URI, callback, params={}) {
+
+	URI += "?"
+	params = [..._entriesOf(params)] // Gets [a, b] for everything in params
+		.map((([key, value]) => 
+		{return `${key}=${value}`})) // Then for each of them put it in this string
+	
+	URI += params.join("&")
+	
+	return genericAPI({
+		"action": "getURI",
+		"uri": URI,
+		"callback": callback
+	})
+
+}
